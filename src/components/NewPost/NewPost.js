c68e0f61dad7ea684cac9fe06a4128340f5d5416
Renan Silva <renaneduardo1@hotmail.com>
2019-02-25 15:54:19 -0300

    Módulo finalizado

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 import './NewPost.css';
+import axios from 'axios';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.body,
+            author: this.state.author
+        };
+        axios.post('/posts', data)
+            .then(response => {
+                console.log(response)
+            });
     }
 
     render () {
@@ -21,8 +34,9 @@ class NewPost extends Component {
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
+                    <option value="Renan">Renan</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
